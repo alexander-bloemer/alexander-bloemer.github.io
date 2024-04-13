418d16c91c06d584a58520420874682963064c1f
AlexanderBloemer <alexander.bloemer@tum.de>
2024-04-13 17:02:32 -0400

    fix themesetting

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -201,7 +201,16 @@ let determineThemeSetting = () => {
   if (themeSetting != "dark" && themeSetting != "light" && themeSetting != "system") {
     themeSetting = "system";
   }
-  return themeSetting;
+  if (themeSetting == "system") {
+    const userPref = window.matchMedia;
+    if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    } else {
+      return "light";
+    }
+  } else {
+    return themeSetting;
+  }
 };
 
 // Determine the computed theme, which can be "dark" or "light". If the theme setting is
